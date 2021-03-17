92d5f66582173ed722dce74d9a1bb54f82e384d1
shota.nobukuni <gsnysh4073@gmail.com>
2021-03-17 23:25:50 +0900

    add manageRewardView.html

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, RewardListView
 urlpatterns = [
+    path('list/', RewardListView.as_view(), name='list'),
     path('', IndexView.as_view(), name='index'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import QuestRewardModel, StudyTimeModel
 
 
 class IndexView(TemplateView):
     template_name = 'app/index.html'
+
+
+class RewardListView(ListView):
+    template_name = 'app/manageRewardView.html'
+    model = QuestRewardModel
